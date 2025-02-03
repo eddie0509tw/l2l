f3a2d032d1482e5e5f006a6f2013012895cc9a15
eddie0509tw <eddie880509@gmail.com>
2025-02-03 01:10:53 -0500

    update

--- docker.sh
+++ docker.sh
@@ -1,7 +1,11 @@
-$IMAGE_NAME="mamltest"
-$IMAGE_TAG="latest"
+#!/bin/bash
+IMAGE_NAME="mamltest"
+IMAGE_TAG="latest"
 
-rocker --nvidia --x11 --network host   \
- --volume .:/workspace  --volume /mnt/datasets:/workspace/datasets   \
-  --env DISPLAY=$DISPLAY  --env XAUTHORITY=/root/.Xauthority  \
-    --  $IMAGE_NAME:$IMAGE_TAG 
+# rocker --nvidia --x11 --network host   \
+#  --volume .:/workspace  --volume /mnt/datasets:/workspace/datasets   \
+#   --env DISPLAY=$DISPLAY  --env XAUTHORITY=/root/.Xauthority  \
+#     --  $IMAGE_NAME:$IMAGE_TAG 
+docker run -it --rm --gpus all --ipc=host --ulimit memlock=-1 --network="host"  \
+   -e DISPLAY=$DISPLAY     -v /tmp/.X11-unix:/tmp/.X11-unix     -v $HOME/.Xauthority:/root/.Xauthority  \
+    -v ./:/workspace     -v /mnt/:/workspace/dataset    $IMAGE_NAME:$IMAGE_TAG
